23cd98b7f5df204e6e25adaae65256112b5589bb
infotowasim <infotowasim@gmail.com>
2026-06-06 17:58:36 +0530

    sonar-qube-perform

--- auth-service/src/test/java/com/kafka/mart/auth/controller/AuthControllerTest.java
+++ auth-service/src/test/java/com/kafka/mart/auth/controller/AuthControllerTest.java
@@ -11,9 +11,8 @@ import com.kafka.mart.auth.service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -35,13 +34,13 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private JwtService jwtService;
 
-    @MockBean
+    @MockitoBean
     private JwtFilter jwtFilter;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
 
